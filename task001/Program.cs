8375bb664c7931e43adb92f0b7ad9d464f954102
Sergey <zabelich19@gmail.com>
2023-06-18 21:07:08 +0300

    Д/З №2 C# 2 цифра 3-х значного числа

---
+++ task001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. ");
+Console.Write("Введи любое трёхзначное число: ");
+int Number = int.Parse(Console.ReadLine());
+string number = Convert.ToString(Number);
+Console.WriteLine("Вторая цифра этого числа: "+number[1]);
+
