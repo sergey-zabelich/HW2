86a59ac29a324c832c0b39b8a848d52ede83ee77
Sergey <zabelich19@gmail.com>
2023-06-18 21:22:20 +0300

    Д/З №2 C# 3 цифра числа и 3 цифры нет

---
+++ task002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. ");
+
+Console.Write("Введи число: ");
+int Number = int.Parse(Console.ReadLine());
+string number = Convert.ToString(Number);
+if (number.Length > 2)
+{
+  Console.WriteLine("третья цифра: " + number[2]);
+}
+else
+  Console.WriteLine("Третьей цифры нет");
