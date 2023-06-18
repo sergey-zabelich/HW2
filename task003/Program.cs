a80af545c148489b4d4f0c8bbb46eb8d0ad7f1ac
Sergey <zabelich19@gmail.com>
2023-06-18 21:38:07 +0300

    Д/З №2 C# дни недели

---
+++ task003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. ");
+Console.Write("Введи цифру, обозначающую день недели: ");
+int daynumber = int.Parse(Console.ReadLine());
+
+void week (int daynumber) {
+  if (daynumber == 6 || daynumber == 7) {
+  Console.WriteLine("Ура!!! Выходной!!! ");
+  }
+  else if (daynumber < 1 || daynumber > 7) {
+    Console.WriteLine("Вы что-то неправильно ввели (В неделе 7 дней)");
+  }
+  else Console.WriteLine("Как жаль что это не выходной =( ");
+}
+
+week(daynumber);
